961a8d50f88516574961376153613a85cfc7a6bc
jmramirez0510 <juanmiguelramirez822@gmail.com>
2025-05-16 12:36:09 -0500

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP CRUD MYSQL BY JUAN MIGUEL RAMIREZ</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  </head>
+  <body>
 
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">PHP MySQL CRUD BY JUAN MIGUEL RAMIREZ</a>
+      </div>
+    </nav>
+</nav>
